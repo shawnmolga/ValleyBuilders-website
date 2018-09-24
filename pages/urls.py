698307344d27d765612c9543daf015d1a065f3a5
shawnmolga <33542884+shawnmolga@users.noreply.github.com>
2018-09-25 01:19:53 +0300

    finished project pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import HomePageView, KitchenPageView, BathroomPageView, FloorsPageView, ExpandingPageView, DrivewaysPageView, RoofingPageView, WindowsPageView, PaintPageView
+from .views import HomePageView, KitchenPageView, BathroomPageView, FloorsPageView, ExpandingPageView, DrivewaysPageView, RoofingPageView, WindowsPageView, PaintPageView, PatiosPageView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('kitchen/', KitchenPageView.as_view(), name='kitchen'),
-    path('bathroom/', BathroomPageView.as_view(), name='bathroom'),
     path('bathroom/', BathroomPageView.as_view(), name='bathroom'),
     path('windows_and_doors/', WindowsPageView.as_view(), name='windows'),
     path('roofing/', RoofingPageView.as_view(), name='roofing'),
@@ -14,6 +13,7 @@ urlpatterns = [
     path('floors/', FloorsPageView.as_view(), name='floors'),
     path('expanding/', ExpandingPageView.as_view(), name='expanding'),
     path('driveways_and_walkways/', DrivewaysPageView.as_view(), name='driveways'),
+    path('patios/', PatiosPageView.as_view(), name='patios'),
     path('', HomePageView.as_view(), name='home')
 
 ]
