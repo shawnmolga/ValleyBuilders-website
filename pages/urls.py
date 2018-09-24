75840968db9be3a9766be99a12f16f00b3338dba
shawnmolga <33542884+shawnmolga@users.noreply.github.com>
2018-09-24 15:48:09 +0300

    adding project pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,19 +1,21 @@
 from django.urls import path
 
-from .views import HomePageView, KitchenPageView, BathroomPageView
+from .views import HomePageView, KitchenPageView, BathroomPageView, FloorsPageView, ExpandingPageView, DrivewaysPageView, RoofingPageView, WindowsPageView, PaintPageView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('kitchen/', KitchenPageView.as_view(), name='kitchen'),
     path('bathroom/', BathroomPageView.as_view(), name='bathroom'),
-    path('windows_and_doors/', BathroomPageView.as_view(), name='windows'),
-    path('roofing/', BathroomPageView.as_view(), name='roofing'),
-    path('paint_jobs/', BathroomPageView.as_view(), name='paint'),
-    path('floors/', BathroomPageView.as_view(), name='floors'),
-    path('expanding/', BathroomPageView.as_view(), name='expanding'),
-    path('driveways_and_walkways/', BathroomPageView.as_view(), name='driveways'),
+    path('bathroom/', BathroomPageView.as_view(), name='bathroom'),
+    path('windows_and_doors/', WindowsPageView.as_view(), name='windows'),
+    path('roofing/', RoofingPageView.as_view(), name='roofing'),
+    path('paint_jobs/', PaintPageView.as_view(), name='paint'),
+    path('floors/', FloorsPageView.as_view(), name='floors'),
+    path('expanding/', ExpandingPageView.as_view(), name='expanding'),
+    path('driveways_and_walkways/', DrivewaysPageView.as_view(), name='driveways'),
     path('', HomePageView.as_view(), name='home')
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
